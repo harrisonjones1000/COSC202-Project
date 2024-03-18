ce26874e081271b02aa938cdb2f6e3c8c09ca337
Ryan <wilry110@student.otago.ac.nz>
2024-03-19 10:16:00 +1300

    Implemented Ability to add Unit tests

---
+++ test/DefaultTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+ 
+//Add imports here for test
+//e.g. import cosc202.andie.ImagePanel;
+
+public class DefaultTest {
+    //Tests if true is true
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+    //Tests if false is false
+    @Test
+    public void testFalse() {
+        assertFalse(false);
+    }
+    //Tests if x = x
+    @Test
+    public void testEquals(){
+        int x = 1;
+        assertEquals(x,x);
+    }
+    @Test
+    //Tests if x =/= y
+    public void testNotEquals(){
+        int x = 1;
+        int y = 2;
+        assertNotEquals(x,y);
+    }
+}
